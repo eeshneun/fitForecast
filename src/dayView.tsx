1b8495164e64076f7793f8bff31613f655614069
eeshneun <148018955+eeshneun@users.noreply.github.com>
2024-02-25 13:16:25 -0500

    day view

--- src/dayView.tsx
+++ src/dayView.tsx
@@ -1,6 +1,27 @@
+import './dayView.css'
 function DayView() {
+
 return <>
-<h1>day view</h1></>
+
+<head>
+    <title>Day View</title>
+</head>
+<body>
+    <div className="headers">
+        <h1>day view</h1>
+        <h2 id="date"> Date </h2>
+        <div className="flexbox-container">
+            <div className="box">top</div>
+            <div className="box">bottom</div>
+            <div className="box">outerwear</div>
+            <div className="box">shoes</div>
+            <div className="box">accessories</div>
+    </div>  
+    </div> 
+</body>
+</>
+
 }
+
 
 export default DayView
